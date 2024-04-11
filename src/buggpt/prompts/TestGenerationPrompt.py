bcb57e29b2cc7394c9afcecc006e2d025bae543b
Michael Pradel <michael@binaervarianz.de>
2024-04-11 09:46:27 -0700

    Using tests+class in test generation prompt

--- src/buggpt/prompts/TestGenerationPrompt.py
+++ src/buggpt/prompts/TestGenerationPrompt.py
@@ -38,10 +38,57 @@ Respond only with Python, i.e., no explanations.
         self.use_json_output = False
 
     def create_prompt(self):
+        prompt_template_code_part = """
+Consider this Python function:
+```python
+<CODE>
+```
 
-        prompt = self.instruction.replace(
+The function may have a bug:
+<HYPOTHESIS>
+
+"""
+
+        prompt_template_tests_part = """
+As context, here are existing tests for the function:
+<TESTS>
+
+"""
+
+        prompt_template_class_part = """
+As context, here is the class that contains the function:
+<CLASS>
+
+"""
+
+        prompt_template_instruction_part = """
+Complete the following template into a complete, self-contained test case that exposes this bug:
+```python
+import unittest
+from my_module import <FUNCTION_NAME>
+# Any other required imports here
+
+# Any mocks or stubs here
+
+class MyTest(unittest.TestCase):
+    def test(self):
+```
+
+Respond only with Python, i.e., no explanations.
+```python
+"""
+        prompt_template = prompt_template_code_part
+        if self.code_context.tests:
+            prompt_template += prompt_template_tests_part
+        if self.code_context.surrounding_class:
+            prompt_template += prompt_template_class_part
+        prompt_template += prompt_template_instruction_part
+
+        prompt = prompt_template.replace(
             "<CODE>", self.code_context.fut).replace(
             "<HYPOTHESIS>", self.hypothesis).replace(
+            "<TESTS>", self.code_context.tests).replace(
+            "<CLASS>", self.code_context.surrounding_class).replace(
             "<FUNCTION_NAME>", self.code_context.fut_name)
         return prompt
 
@@ -51,7 +98,7 @@ Respond only with Python, i.e., no explanations.
         # extract code
         in_code = False
         for line in raw_answer.split("\n"):
-            if line == "```":
+            if line.startswith("```"):
                 break
             if in_code:
                 generated_test += line + "\n"
