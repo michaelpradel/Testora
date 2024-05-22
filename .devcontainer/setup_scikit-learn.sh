490bc23e18df99bafd1703767a6f123ce736e873
Michael Pradel <michael@binaervarianz.de>
2024-05-22 10:26:01 -0700

    three cloned repos for scikit-learn

--- .devcontainer/setup_scikit-learn.sh
+++ .devcontainer/setup_scikit-learn.sh
@@ -23,4 +23,24 @@ docker exec -w /home/scikit-learn scikit-learn-dev1 pip install wheel numpy scip
 docker exec -w /home/scikit-learn scikit-learn-dev1 pip install --editable . --verbose --no-build-isolation --config-settings editable-verbose=true
 echo "Done with first clone"
 
+echo "Creating second clone of scikit-learn"
+cd ../..
+cp -r clone1 clone2
+cd clone2/scikit-learn
+echo "Building dev container for scikit-learn (first clone)"
+docker run -t -d --name scikit-learn-dev2 -v ${PWD}:/home/scikit-learn python:3.10
+docker exec -w /home/scikit-learn scikit-learn-dev2 pip install wheel numpy scipy cython meson-python ninja
+docker exec -w /home/scikit-learn scikit-learn-dev2 pip install --editable . --verbose --no-build-isolation --config-settings editable-verbose=true
+echo "Done with second clone"
+
+echo "Creating third clone of scikit-learn"
+cd ../..
+cp -r clone1 clone3
+cd clone3/scikit-learn
+echo "Building dev container for scikit-learn (first clone)"
+docker run -t -d --name scikit-learn-dev3 -v ${PWD}:/home/scikit-learn python:3.10
+docker exec -w /home/scikit-learn scikit-learn-dev3 pip install wheel numpy scipy cython meson-python ninja
+docker exec -w /home/scikit-learn scikit-learn-dev3 pip install --editable . --verbose --no-build-isolation --config-settings editable-verbose=true
+echo "Done with third clone"
+
 cd ../../../BugGPT
