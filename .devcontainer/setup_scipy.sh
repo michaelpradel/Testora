a9b8c206966543edbe77d702017e92bddbec4df5
Michael Pradel <michael@binaervarianz.de>
2024-05-24 11:27:24 -0700

    improved scipy setup

--- .devcontainer/setup_scipy.sh
+++ .devcontainer/setup_scipy.sh
@@ -21,14 +21,9 @@ git submodule update --init
 echo "Building dev container for scipy (first clone)"
 
 docker run -t -d --name scipy-dev1 -v ${PWD}:/home/scipy python:3.10
-docker exec -w /home/scipy scipy-dev1 apt install -y gcc g++ gfortran libopenblas-dev liblapack-dev pkg-config
-docker exec -w /home/scipy scipy-dev1 wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-docker exec -w /home/scipy scipy-dev1 bash Miniforge3.sh -b -p "${HOME}/conda"
-docker exec -w /home/scipy scipy-dev1 source "${HOME}/conda/etc/profile.d/conda.sh"
-docker exec -w /home/scipy scipy-dev1 source "${HOME}/conda/etc/profile.d/mamba.sh"
-docker exec -w /home/scipy scipy-dev1 mamba env create -f environment.yml
-docker exec -w /home/scipy scipy-dev1 mamba activate scipy-dev
-docker exec -w /home/scipy scipy-dev1 pip install -e . --no-build-isolation
+docker cp /workspaces/BugGPT/.devcontainer/setup_scipy_to_run_in_container.sh scipy-dev1:/root/setup.sh
+docker exec scipy-dev1 chmod +x /root/setup.sh
+docker exec -w /home/scipy scipy-dev1 /root/setup.sh
 echo "Done with first clone"
 
 # todo: 2 more clones
---
+++ .devcontainer/setup_scipy_to_run_in_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt install -y gcc g++ gfortran libopenblas-dev liblapack-dev pkg-config
+wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3.sh -b -p "${HOME}/conda"
+source "${HOME}/conda/etc/profile.d/conda.sh"
+source "${HOME}/conda/etc/profile.d/mamba.sh"
+mamba env create -f environment.yml
+mamba activate scipy-dev
+pip install -e . --no-build-isolation
