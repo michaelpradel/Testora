c2c2f9df2e453e33728c041e66d08560e618e55e
Michael Pradel <michael@binaervarianz.de>
2024-05-24 09:55:10 -0700

    setup script for scipy

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -4,5 +4,6 @@ pip install --user -r requirements.txt
 pip install -e .
 
 echo "Setting up pandas"
-#.devcontainer/setup_pandas.sh
-.devcontainer/setup_scikit-learn.sh
+# .devcontainer/setup_pandas.sh
+# .devcontainer/setup_scikit-learn.sh
+.devcontainer/setup_scipy.sh
