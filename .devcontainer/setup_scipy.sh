201bd9d3da9d43aa749b860cd55e596a05b491a9
Michael Pradel <michael@binaervarianz.de>
2024-06-09 13:42:04 +0200

    adapting scipy script to three nested containers

--- .devcontainer/setup_scipy.sh
+++ .devcontainer/setup_scipy.sh
@@ -19,13 +19,32 @@ git clone https://github.com/scipy/scipy.git
 cd scipy
 git submodule update --init
 echo "Building dev container for scipy (first clone)"
-
 docker run -t -d --name scipy-dev1 -v ${PWD}:/home/scipy python:3.10
 docker cp /workspaces/BugGPT/.devcontainer/setup_scipy_to_run_in_container.sh scipy-dev1:/root/setup.sh
 docker exec scipy-dev1 chmod +x /root/setup.sh
 docker exec -w /home/scipy scipy-dev1 /root/setup.sh
 echo "Done with first clone"
 
-# todo: 2 more clones
+echo "Creating second clone of scipy"
+cd ../..
+cp -r clone1 clone2
+cd clone2/scipy
+echo "Building dev container for scipy (second clone)"
+docker run -t -d --name scipy-dev2 -v ${PWD}:/home/scipy python:3.10
+docker cp /workspaces/BugGPT/.devcontainer/setup_scipy_to_run_in_container.sh scipy-dev2:/root/setup.sh
+docker exec scipy-dev2 chmod +x /root/setup.sh
+docker exec -w /home/scipy scipy-dev2 /root/setup.sh
+echo "Done with second clone"
+
+echo "Creating third clone of scipy"
+cd ../..
+cp -r clone1 clone3
+cd clone3/scipy
+echo "Building dev container for scipy (third clone)"
+docker run -t -d --name scipy-dev3 -v ${PWD}:/home/scipy python:3.10
+docker cp /workspaces/BugGPT/.devcontainer/setup_scipy_to_run_in_container.sh scipy-dev3:/root/setup.sh
+docker exec scipy-dev3 chmod +x /root/setup.sh
+docker exec -w /home/scipy scipy-dev3 /root/setup.sh
+echo "Done with third clone"
 
 cd ../../../BugGPT
