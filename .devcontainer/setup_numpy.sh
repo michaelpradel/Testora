fbc327ad2a45673e7bad5fc6b02190124c402e8d
Michael Pradel <michael@binaervarianz.de>
2024-06-11 11:30:16 +0200

    1 --> 3 inner containers for numpy

--- .devcontainer/setup_numpy.sh
+++ .devcontainer/setup_numpy.sh
@@ -25,6 +25,27 @@ docker exec numpy-dev1 chmod +x /root/setup.sh
 docker exec -w /home/numpy numpy-dev1 /root/setup.sh
 echo "Done with first clone"
 
-# TODO: 2 more clones
+echo "Creating second clone of numpy"
+cd ../..
+cp -r clone1 clone2
+cd clone2/numpy
+echo "Building dev container for numpy (second clone)"
+docker run -t -d --name numpy-dev2 -v ${PWD}:/home/numpy python:3.10
+docker cp /workspaces/BugGPT/.devcontainer/setup_numpy_to_run_in_container.sh numpy-dev2:/root/setup.sh
+docker exec numpy-dev2 chmod +x /root/setup.sh
+docker exec -w /home/numpy numpy-dev2 /root/setup.sh
+echo "Done with second clone"
+
+echo "Creating third clone of numpy"
+cd ../..
+cp -r clone1 clone3
+cd clone3/numpy
+echo "Building dev container for numpy (third clone)"
+docker run -t -d --name numpy-dev3 -v ${PWD}:/home/numpy python:3.10
+docker cp /workspaces/BugGPT/.devcontainer/setup_numpy_to_run_in_container.sh numpy-dev3:/root/setup.sh
+docker exec numpy-dev3 chmod +x /root/setup.sh
+docker exec -w /home/numpy numpy-dev3 /root/setup.sh
+echo "Done with third clone"
+
 
 cd ../../../BugGPT
