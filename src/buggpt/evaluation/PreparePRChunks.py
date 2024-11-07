efb54c6b14dec3096ab20cdd71d80f8d7013cf07
Michael Pradel <michael@binaervarianz.de>
2024-11-07 10:31:52 +0100

    adding to list of target PRs

--- src/buggpt/evaluation/PreparePRChunks.py
+++ src/buggpt/evaluation/PreparePRChunks.py
@@ -9,10 +9,10 @@ def write_PR_tasks_into_database(project_name, project_id):
     github = Github(auth=Auth.Token(token))
     
     github_repo = github.get_repo(project_id)
-    github_prs = get_recent_prs(github_repo, nb=90)
+    github_prs = get_recent_prs(github_repo, nb=150)
     recent_pr_nbs = [pr.number for pr in github_prs]
 
-    # recent_pr_nbs = [n for n in recent_pr_nbs if n < 2255]
+    recent_pr_nbs = [n for n in recent_pr_nbs if n < 2195]
 
     EvalTaskManager.write_tasks(project_name, recent_pr_nbs)
 
