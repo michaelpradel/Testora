c978ee73a9d4234edf602b032a22ac9c54915496
Michael Pradel <michael@binaervarianz.de>
2024-11-07 10:17:16 +0100

    starting to build a list of target PRs

--- src/buggpt/evaluation/PreparePRChunks.py
+++ src/buggpt/evaluation/PreparePRChunks.py
@@ -9,16 +9,18 @@ def write_PR_tasks_into_database(project_name, project_id):
     github = Github(auth=Auth.Token(token))
     
     github_repo = github.get_repo(project_id)
-    github_prs = get_recent_prs(github_repo, nb=30)
+    github_prs = get_recent_prs(github_repo, nb=90)
     recent_pr_nbs = [pr.number for pr in github_prs]
+
+    # recent_pr_nbs = [n for n in recent_pr_nbs if n < 2255]
 
     EvalTaskManager.write_tasks(project_name, recent_pr_nbs)
 
 
 if __name__ == "__main__":
-    write_PR_tasks_into_database("pandas", "pandas-dev/pandas")
-    write_PR_tasks_into_database("scipy", "scipy/scipy")
-    write_PR_tasks_into_database("keras", "keras-team/keras")
+    # write_PR_tasks_into_database("pandas", "pandas-dev/pandas")
+    # write_PR_tasks_into_database("scipy", "scipy/scipy")
+    # write_PR_tasks_into_database("keras", "keras-team/keras")
     write_PR_tasks_into_database("marshmallow", "marshmallow-code/marshmallow")
     
     # write_PR_tasks_into_database("scikit-learn", "scikit-learn/scikit-learn")
