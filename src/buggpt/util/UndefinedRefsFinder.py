54ad751119d7b1db12cc6fd4b7923f2ce7282767
Michael Pradel <michael@binaervarianz.de>
2024-11-15 09:58:52 +0100

    switching to libcst to find undefined variables

--- src/buggpt/util/UndefinedRefsFinder.py
+++ src/buggpt/util/UndefinedRefsFinder.py
@@ -1,67 +1,30 @@
-import ast
-import builtins
+import libcst as cst
 
 
-class NameCollector(ast.NodeVisitor):
-    def __init__(self):
-        self.scopes = [set()]
-        self.used_names = set()
+def get_undefined_references(src):
+    undefined_variables = []  # using a list here to get a deterministic order
 
-    def visit_Name(self, node):
-        if isinstance(node.ctx, ast.Store):
-            # Name is being assigned to; add to current scope
-            self.scopes[-1].add(node.id)
-        elif isinstance(node.ctx, ast.Load):
-            # Name is being used; add to used names
-            self.used_names.add(node.id)
-        self.generic_visit(node)
+    ast = cst.parse_module(src)
+    ast_wrapper = cst.metadata.MetadataWrapper(ast)
+    scopes = ast_wrapper.resolve(cst.metadata.ScopeProvider).values()
+    for scope in scopes:
+        for access in scope.accesses:
+            if len(access.referents) == 0:
+                node = access.node
+                undefined_variables.append(node.value)
 
-    def visit_Import(self, node):
-        for alias in node.names:
-            name = alias.asname if alias.asname else alias.name
-            self.scopes[-1].add(name.split('.')[0])
+    # remove duplicates
+    undefined_variables = list(dict.fromkeys(undefined_variables))
 
-    def visit_ImportFrom(self, node):
-        for alias in node.names:
-            name = alias.asname if alias.asname else alias.name
-            self.scopes[-1].add(name)
-
-    def visit_ExceptHandler(self, node):
-        if node.name:
-            self.scopes[-1].add(node.name)
-        self.generic_visit(node)
-
-    def visit_ClassDef(self, node):
-        # Add class name to current scope
-        self.scopes[-1].add(node.name)
-        self.generic_visit(node)
-
-    def visit_FunctionDef(self, node):
-        # Add function name to current scope
-        self.scopes[-1].add(node.name)
-        self.generic_visit(node)
-
-
-def get_undefined_references(code):
-    # Parse the code into an AST
-    tree = ast.parse(code)
-    collector = NameCollector()
-    collector.visit(tree)
-
-    # Combine all defined names from all scopes
-    defined_names = set().union(*collector.scopes)
-
-    # Undefined names are used names not in defined names or builtins
-    undefined_names = collector.used_names - defined_names - set(dir(builtins))
-    return list(undefined_names)
+    return undefined_variables
 
 
 if __name__ == "__main__":
     code = """
-def foo():
-    pass
+def foo(l):
+    l()
 
-foo()
+foo(lambda n: print(n), bar)
 """
     undefined_refs = get_undefined_references(code)
     print("Undefined references:", undefined_refs)
