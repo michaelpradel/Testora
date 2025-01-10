b7c00155540ac0931ee3b07241d32819b71bb92a
Michael Pradel <michael@binaervarianz.de>
2025-01-10 10:52:32 +0100

    quick check of variance of results

--- src/buggpt/evaluation/ClassificationResultsSummarizer.py
+++ src/buggpt/evaluation/ClassificationResultsSummarizer.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import json
 from buggpt.evaluation.ResultsManager import result_files_for_project
 
@@ -7,6 +8,7 @@ nb_fps = 0
 nb_tps = 0
 nb_fns = 0
 nb_tns = 0
+variance_ctr = Counter()
 for project in ["keras"]: #, "marshmallow", "scipy", "pandas"]:
     for result_file in result_files_for_project(project, is_classification=True):
         with open(result_file, "r") as f:
@@ -41,6 +43,11 @@ for project in ["keras"]: #, "marshmallow", "scipy", "pandas"]:
                         raise ValueError(f"Invalid label: {
                                          entry['label']}, {entry["pr_nb"]}")
 
+                    # check variance of predictions
+                    results_counter = Counter(results)
+                    variance_str = str(sorted(list(results_counter.values()), reverse=True))
+                    variance_ctr[variance_str] += 1
+
                     # print into CSV
                     print(f"{project}, "
                           f"{entry['pr_nb']}, "
@@ -54,3 +61,5 @@ precision = 0 if (nb_tps + nb_fps) == 0 else nb_tps / (nb_tps + nb_fps)
 print(f"Precision: {precision}")
 recall = 0 if (nb_tps + nb_fns) == 0 else nb_tps / (nb_tps + nb_fns)
 print(f"Recall: {recall}")
+print()
+print(f"Variance of predictions: {variance_ctr}")
