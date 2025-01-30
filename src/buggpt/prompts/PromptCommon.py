f1280aa20470d7cabf70293f242bf80c5a250b1a
Michael Pradel <michael@binaervarianz.de>
2025-01-30 11:25:34 +0100

    no system message for DeepSeek

--- src/buggpt/prompts/PromptCommon.py
+++ src/buggpt/prompts/PromptCommon.py
@@ -1,4 +1,9 @@
+from buggpt.Config import model_version
+
 # NOTE: when changing the system message, must remove the old cache
-system_message = """
-You are an experienced Python developer.
-"""
+
+if model_version.startswith("gpt"):
+    system_message = "You are an experienced Python developer."
+elif model_version.startswith("deepseek"):
+    system_message = ""
+
